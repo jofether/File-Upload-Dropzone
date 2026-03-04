cbd146641f2dbc609cf11edf4f2dda46e69537a1
spaghettieater <jofethersampollo@gmail.com>
2026-03-04 14:51:05 +0800

    Playwright Ready

---
+++ playwright_actions.js
@@ -0,0 +1,18 @@
+export const actions = [
+  {
+    name: "bug-6-AddFileUpload",
+    selector: ".border-2.border-dashed.rounded-xl"
+  },
+  {
+    name: "bug-8-AddFileUpload",
+    selector: ".border-2.border-dashed.rounded-xl"
+  },
+  {
+    name: "bug-9-AddFileUpload",
+    selector: ".border-2.border-dashed.rounded-xl"
+  },
+  {
+    name: "bug-10-AddFileUpload",
+    selector: ".border-2.border-dashed.rounded-xl"
+  }
+];
